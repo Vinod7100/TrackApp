dabee2d21cccd59abd87126fad75ec930c23b2bb
Vinod7100 <vinod.hostzack@gmail.com>
2015-07-20 15:41:01 +0530

    links made working without function. Added Buttons and icons to context menu, and added a modal to add number button.

--- js/controllers.js
+++ js/controllers.js
@@ -5,10 +5,6 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 /****** Login Page controller *****/
 phonecatControllers.controller('loginPageCtrl', ['$scope', '$http', '$location',
 	function($scope, $http, $location) { 
-		$scope.showPage = function(pathurl){
-			console.log(pathurl);
-			$location.path(pathurl)
-		}
 		$scope.submit = function(){
 			$scope.loading = true;
 			var pathurl = "/home";
@@ -20,10 +16,6 @@ phonecatControllers.controller('loginPageCtrl', ['$scope', '$http', '$location',
 /****** Registration Page controller *****/
 phonecatControllers.controller('registrationPageCtrl', ['$scope', '$http', '$location',
 	function($scope, $http, $location) {  
-		$scope.showPage = function(pathurl){
-			console.log(pathurl);
-			$location.path(pathurl)
-		}
 		$scope.submit = function(){
 			$scope.loading = true;
 		}
@@ -33,10 +25,6 @@ phonecatControllers.controller('registrationPageCtrl', ['$scope', '$http', '$loc
 /****** Forgot Password Page controller *****/
 phonecatControllers.controller('forgotPasswordPageCtrl', ['$scope', '$http', '$location',
 	function($scope, $http, $location) {  
-		$scope.showPage = function(pathurl){
-			console.log(pathurl);
-			$location.path(pathurl)
-		}
 		$scope.submit = function(){
 			$scope.loading = true;
 		}
@@ -44,14 +32,49 @@ phonecatControllers.controller('forgotPasswordPageCtrl', ['$scope', '$http', '$l
 ]);
 
 /****** Home Page controller *****/
-phonecatControllers.controller('homePageCtrl', ['$scope', '$http', '$location',
-	function($scope, $http, $location) {  
-		$scope.showPage = function(pathurl){
-			console.log(pathurl);
-			$location.path(pathurl)
-		}
+phonecatControllers.controller('homePageCtrl', ['$scope', '$http', '$modal', '$location',
+	function($scope, $http, $modal, $location) {  
 		$scope.submit = function(){
 			$scope.loading = true;
 		}
+		$scope.items = ['item1', 'item2', 'item3'];
+		$scope.animationsEnabled = true;
+		$scope.open = function (size) {
+
+			var modalInstance = $modal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'add_number.html',
+				controller: 'ModalInstanceCtrl',
+				size: size,
+				resolve: {
+					items: function () {
+						return $scope.items;
+					}
+				}
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+				$scope.selected = selectedItem;
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
+	}
+]);
+phonecatControllers.controller('ModalInstanceCtrl', ['$scope', '$http', 'items', '$modalInstance', '$location',
+	function($scope, $http, items, $modalInstance, $location) {  
+		$scope.submit = function(){
+			$scope.loading = true;
+		}
+		$scope.items = items;
+		$scope.selected = {
+			item: $scope.items[0]
+		};
+		$scope.ok = function () {
+			$modalInstance.close($scope.selected.item);
+		};
+		$scope.cancel = function () {
+			$modalInstance.dismiss('cancel');
+		};
 	}
 ]);
